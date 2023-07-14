6840db44d2cfd38bfe8e58d1881b6703e1c5337e
Gryu <125647772+GRyu1@users.noreply.github.com>
2023-07-14 19:15:42 +0900

    1234
    1234

--- src/main/java/com/sh/hairball/animal/model/dao/AnimalDao.java
+++ src/main/java/com/sh/hairball/animal/model/dao/AnimalDao.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 import com.sh.hairball.animal.model.exception.AnimalException;
@@ -13,6 +14,8 @@ import com.sh.hairball.animal.model.vo.Animal;
 import com.sh.hairball.animal.model.vo.AnimalType;
 import com.sh.hairball.animal.model.vo.Sex;
 import com.sh.hairball.board.adoptboard.model.dao.AdoptionDao;
+import com.sh.hairball.board.adoptboard.model.exception.AdopBoardException;
+import com.sh.hairball.board.enrollboard.model.vo.EnrollBoard;
 
 public class AnimalDao {
 	Properties prop = new Properties();
@@ -102,5 +105,26 @@ public class AnimalDao {
 		}
 		
 		return boardNo;
+	}
+
+	
+	public int getTotalContent(Connection conn) {
+		int totalCnt = 0;
+		String sql = prop.getProperty("getTotalContent");
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			 try (ResultSet rset = pstmt.executeQuery()) {
+				 while(rset.next()) {
+					 totalCnt = rset.getInt("count(*)");
+				 }
+			 } 
+		} catch (SQLException e) {
+			throw new AdopBoardException("전체 게시글 조회 실패", e);
+		}
+		return totalCnt;
+	}
+
+	public List<EnrollBoard> findList(int start, int end) {
+		
+		return null;
 	}
 }
--- src/main/java/com/sh/hairball/animal/model/service/AnimalService.java
+++ src/main/java/com/sh/hairball/animal/model/service/AnimalService.java
@@ -1,5 +1,29 @@
 package com.sh.hairball.animal.model.service;
 
+import static com.sh.hairball.common.JdbcTemplate.close;
+import static com.sh.hairball.common.JdbcTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.sh.hairball.animal.model.dao.AnimalDao;
+import com.sh.hairball.board.enrollboard.model.vo.EnrollBoard;
+
 public class AnimalService {
+	private final AnimalDao animalDao = new AnimalDao();
+
+	public List<EnrollBoard> findList(int start, int end) {
+
+		
+		List<EnrollBoard> result = animalDao.findList(start, end);
+		return result;
+	}
+
+	public int getTotalContent() {
+		Connection conn = getConnection();
+		int total = animalDao.getTotalContent(conn);
+		close(conn);
+		return total;
+	}
 
 }
