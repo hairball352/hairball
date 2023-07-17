8be2afe4a9d9a043e9eddd4371d12ef5cb0eea3e
Jeonyera <vldzmf0877@naver.com>
2023-07-17 11:58:02 +0900

    커밋

---
+++ src/main/java/com/sh/hairball/webchat/controller/AdminChatServlet.java
@@ -0,0 +1,26 @@
+package com.sh.hairball.webchat.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminChatServlet
+ */
+@WebServlet("/admin/AdminChat")
+public class AdminChatServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/views/admin/AdminChat.jsp").forward(request, response);
+	
+	}
+
+}
