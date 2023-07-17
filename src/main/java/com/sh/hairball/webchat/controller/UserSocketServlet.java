be4ab4a956b094815dc70098c47bcf5ca73cc29e
Jeonyera <vldzmf0877@naver.com>
2023-07-17 11:53:12 +0900

    커밋

--- src/main/java/com/sh/hairball/webchat/controller/UserSocketServlet.java
+++ src/main/java/com/sh/hairball/webchat/controller/UserSocketServlet.java
@@ -10,6 +10,8 @@ public class UserSocketServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	
+    	String loginMemberWebChat = req.getParameter(getServletName());
         req.getRequestDispatcher("/WEB-INF/views/webChat/user.jsp").forward(req, resp);
 
     }
