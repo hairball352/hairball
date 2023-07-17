e93f41d5829c546955a1f8747df0b51a9e62adcc
Jeonyera <vldzmf0877@naver.com>
2023-07-17 11:57:44 +0900

    커밋

--- src/main/java/com/sh/hairball/webchat/controller/UserSocketServlet.java
+++ src/main/java/com/sh/hairball/webchat/controller/UserSocketServlet.java
@@ -3,15 +3,24 @@ package com.sh.hairball.webchat.controller;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+
+import com.sh.hairball.member.model.service.MemberService;
+import com.sh.hairball.member.model.vo.Member;
+
 import java.io.IOException;
 
 @WebServlet("/user")
 public class UserSocketServlet extends HttpServlet {
+	
+	private final MemberService memberService = new MemberService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     	
-    	String loginMemberWebChat = req.getParameter(getServletName());
+    	String loginMemberName  = req.getParameter("Name");
+    	
+		Member member = memberService.findById(memberId);
+
         req.getRequestDispatcher("/WEB-INF/views/webChat/user.jsp").forward(req, resp);
 
     }
